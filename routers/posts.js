f0ed5ef56ce0358ac917752be972fbbc787c1611
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-10-16 16:50:10 +0200

    testate con successo le risposte su Postman

--- routers/posts.js
+++ routers/posts.js
@@ -7,29 +7,31 @@ const router = express.Router();
 // • INDEX (nome rotta) -> GET (metodo) /posts (endpoint)
 router.get("/", (req, res) => {
   res.send("Lista dei post");
-});
+}); //GET http://localhost:3000/posts → “Lista dei post”
 
 // • SHOW   -> GET /posts/:id
 router.get("/:id", (req, res) => {
   res.send("Dettaglio del post" + req.params.id);
-});
+}); //GET http://localhost:3000/posts/1 → “Dettaglio del post 1”
 
 // • STORE  -> POST /posts
 router.post("/", (req, res) => {
   res.send("Creazione nuovo post");
-});
+}); //POST http://localhost:3000/posts → “Creazione nuovo post”
 
 // • UPDATE -> PUT /posts/:id
 router.put("/:id", (req, res) => {
   res.send("Modifica integrale del post" + req.params.id);
-});
+}); //PUT http://localhost:3000/posts/1 → “Modifica integrale del post 1”
 
 // • MODIFY -> PATCH /posts/:id
 router.patch("/:id", (req, res) => {
   res.send("Modifica parziale del post" + req.params.id);
-});
+}); //PUT http://localhost:3000/posts/1 → “Modifica integrale del post 1”
 
 // • DESTROY-> DELETE /posts/:id
 router.delete("/:id", (req, res) => {
   res.send("Cancellazione del post" + req.params.id);
-});
+}); //DELETE http://localhost:3000/posts/1 → “Cancellazione del post 1”
+
+module.exports = router;
